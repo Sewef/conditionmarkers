26103bb1f58f2717d962b27f0aef9057652f4fd1
Sewef <27089398+Sewef@users.noreply.github.com>
2026-02-27 23:36:58 +0100

    Lock the text item

--- src/helpers.ts
+++ src/helpers.ts
@@ -252,6 +252,7 @@ export async function setConditionMarkerNumber(
         .textType("PLAIN")
         .position(marker.position)
         .attachedTo(marker.id)
+        .locked(true)
         .layer("ATTACHMENT")
         .fontSize(fontSize)
         .textAlign("CENTER")
